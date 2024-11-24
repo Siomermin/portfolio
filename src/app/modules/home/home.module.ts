921a61a2d0293e94562f435cedafcb65fceb20fe
Siomermin <fermingonzalez011@gmail.com>
2024-11-24 18:03:11 -0300

    Refactorizacion del home + estilos componentes

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,10 +5,10 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ProfileComponent } from './components/profile/profile.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { EducationComponent } from './components/education/education.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 @NgModule({
